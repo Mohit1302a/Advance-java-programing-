eb620c634a8e2d8eaeb6d54c570844692b667f54
abhinendra pratap <123157777+Mohit1302a@users.noreply.github.com>
2026-03-19 01:26:52 +0530

    Add files via upload

---
+++ Filter2.java
@@ -0,0 +1,17 @@
+package streamApi;
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.function.Predicate;
+
+public class Filter2 {
+	public static void main(String[]args) {
+	   List <String> l2 = Arrays.asList("Amit","Ravi","Anil","Vikas","Ashok","Rahul");
+	      ArrayList<String> list=new ArrayList<String>();
+	      list.addAll(l2);
+	      
+	     
+	   
+//	   		         Stream<String> l1Stream = l2.stream();
+
+}
+}
---
+++ Filter3.java
@@ -0,0 +1,12 @@
+package streamApi;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Filter3 {
+    public static void main(String[] args) {
+        List<String> data = Arrays.asList("Amit", "Ravi", "Anil", "Vikas", "Ashok", "Rahul");
+        List<String> filteredData = data.stream().filter((s) -> s.startsWith("A")).toList();
+        System.out.println(filteredData);
+    }
+}
